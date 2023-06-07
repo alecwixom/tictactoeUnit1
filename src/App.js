c86fbdcbc487c147bdb6c4c1b3d45b72ac2f3690
Alec Wixom <atwixom@gmail.com>
2023-06-06 21:56:02 -0600

    Finished my project

--- src/App.js
+++ src/App.js
@@ -7,16 +7,55 @@ import Square from './components/Square';
 function App() {
   const [squares, setSquares] = useState(["","","","","","","","",""])
   const [player, setPlayer] = useState(true)
+  const handlClick = () => {
+    setSquares(["","","","","","","","",""])
+    setPlayer(true)
+  }
   return (
     <div className="App">
-      <Square
-      squares={squares}
-      setSquares={setSquares}
-      player={player}
-      setPlayer={setPlayer}
-      />
+      <div className='container'>
+        {squares.map((val, index) => {
+          return (
+            <Square
+              setSquares={setSquares}
+              index={index}
+              squareValue={val}
+              squares={squares}
+              player={player}
+              setPlayer={setPlayer}
+            />
+          )
+        })}
+      </div>
+      <button onClick={handlClick}>Reset</button>
+      <br/>
+      <br/>
+      <span>{calculateWinner(squares)}</span>
     </div>
   );
+}
+function calculateWinner (arr) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (
+      arr[a] &&
+      arr[a] === arr[b] &&
+      arr[a] === arr[c]
+    ) {
+        return `${arr[a]} won!`
+    }
+  }
+  return "Who will win?"
 }
 
 export default App;
